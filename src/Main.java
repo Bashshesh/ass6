c8e6a7b27f95e889b357bac85e7e014c96fd2f1a
Bashshesh <115872700+Bashshesh@users.noreply.github.com>
2023-05-31 16:00:27 +0600

    i love this world

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,22 @@ public class Main {
         temp.addVertex(3);
         temp.addVertex(2);
         temp.addVertex(4);
+        //adding all vertices
+
         temp.addEdge(1, 3);
         temp.addEdge(1, 2);
-        temp.addEdge(2, 3);
+        temp.addEdge(2, 3); //creating all connections and edges
         temp.addEdge(3, 4);
         temp.printGraph();
+        //print connecthions in whole graph
+        System.out.println();
+
         temp.removeEdge(2,3);
         temp.printGraph();
-        System.out.println(temp.hasEdge(1,3));
-        System.out.println(temp.hasEdge(1,2));
+        //print it after we remove edge 2-3
+        System.out.println(temp.hasEdge(2,3));
+        System.out.println(temp.hasEdge(1,3)); //cheking that we have the edge 1-3(true)
+        System.out.println(temp.hasEdge(1,2)); //chekking that we have the edge 1-2(true)
         System.out.println(temp.getNeighbors(1));
         temp.DFS(1);
     }
